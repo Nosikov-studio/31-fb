6285262769724a0711b3c2e70d15dc551d7778e7
Nosikov <89134038088@mail.ru>
2024-06-09 08:59:18 +0700

    09-06-2024

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhSjG0cq1y1qFv0DfQMHLcOVo6vGnBm64",
+  authDomain: "auth-2f79d.firebaseapp.com",
+  projectId: "auth-2f79d",
+  storageBucket: "auth-2f79d.appspot.com",
+  messagingSenderId: "817347614901",
+  appId: "1:817347614901:web:18becb2c0d75d47ad646fc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
